f0a52b35638f47e66752bdb0c9d4d2327e62178d
Dina <dina.bahnasy@gmail.com>
2025-08-09 03:02:03 +0300

    fix(vercel): correct dynamic import relative paths to ./api/index.js from nested routes

--- api/campaigns/[id].js
+++ api/campaigns/[id].js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const mod = await import(new URL('../../index.js', import.meta.url).href);
+  const mod = await import(new URL('../index.js', import.meta.url).href);
   return mod.default(req, res);
 }
 
